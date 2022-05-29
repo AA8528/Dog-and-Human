117e93fa461f2ccef0d560c55048aba69c2d2344
Adarsh <91724820+AA8528@users.noreply.github.com>
2022-05-29 17:03:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let dog_age= prompt("what is your dog's age?");
+alert("To convert dog age to human age we have to subtract it from 2 and multiply it with 4 and add 21")
+let Human_age= (dog_age - 2) * 4 + 21;
+
+function humanAge(){
+    alert("The human age is: " + Human_age)
+}
+
+
+humanAge()
